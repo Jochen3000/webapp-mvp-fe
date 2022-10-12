0c355029aff7ad55aeabe6ab645139019a1f6bd7
jochen ade <jochen.ade@hotmail.com>
2022-10-12 15:06:08 +0200

    menu working

--- custom.js
+++ custom.js
@@ -10,42 +10,28 @@ const viewWallet = document.querySelector('.view-wallet');
 const viewStore = document.querySelector('.view-store');
 
 //set dashboard menu as active
-menuItem[3].classList.add('active');
+menuItem[0].classList.add('active');
 
 
 // make menu items clickable
-menuItem.forEach((item) => {
-    item.addEventListener("click", (menuclick) => {
-        // hide all views
-        mainViews.forEach((item) => {
-            item.classList.add('hide');
-        });
+for (let i = 0; i < menuItem.length; i++) {
+    menuItem[i].addEventListener("click", () => {
         // set all menu items to unfocused
         menuItem.forEach((item) => {
             item.classList.remove('active');
         });
-        console.log('hallo', item);
-        console.log('huhu', mainViews);
+        // hide all views
+        mainViews.forEach((item) => {
+            item.classList.add('hide');
+        });
+        // display selected 
+        menuItem[i].classList.add('active');
+        mainViews[i].classList.remove('hide');
     });
-})
+}
 
 
-// for (let i = 0; i < menuItem.length; i++) {
-//     menuItem[i].addEventListener('click', () => {
-//         // hide all views
-//         mainViews.forEach((item) => {
-//             item.classList.add('hide');
-//         })
-//         console.log('huhu', mainViews);
 
-//         // show selected view
-//         if (menuItem[i] = 0) {
-
-//         }
-//         console.log('hallo', menuItem[i]);
-//     });
-
-// }
 
 
 
