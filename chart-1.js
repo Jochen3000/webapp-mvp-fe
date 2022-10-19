ae107afa4ba074c81a813229cdc84aed5c40a8d3
Jochen3000 <109900575+Jochen3000@users.noreply.github.com>
2022-10-19 08:20:09 +0200

    barchart done

--- chart-1.js
+++ chart-1.js
@@ -1,28 +1,17 @@
-const ctx = document.getElementById('myChart');
-const myChart = new Chart(ctx, {
+const ctx1 = document.getElementById('barChart');
+const barChart = new Chart(ctx1, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['01', '02', '03', '04', '05', '06'],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            data: [200, 210, 310, 300, 200, 320],
+            backgroundColor: 'rgba(255, 255, 255, 1)',
+            borderColor: 'rgba(255, 255, 255, 1)',
+            borderWidth: 1,
+            barThickness: 8,
+            borderRadius: 5,
+            borderSkipped: false
         }]
     },
     options: {
@@ -30,6 +19,19 @@ const myChart = new Chart(ctx, {
             y: {
                 beginAtZero: true
             }
-        }
+        },
+        scales: {
+    y: {
+      ticks: { color: '#FFFFFF'}
+    },
+    x: {
+      ticks: { color: '#FFFFFF'}
+    }
+  },
+        plugins: {
+         legend: {
+            display: false
+         }
+      }
     }
 });
